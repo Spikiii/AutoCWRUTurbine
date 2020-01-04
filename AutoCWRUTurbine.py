8a0a02d525c1e889f81f2ce626c5a653aaec2e6b
Eytan Kaplan <eytankaplan101@gmail.com>
2020-01-04 12:36:02 -0500

    Stipped out personal information to make public
    The bot now reads the IDs from a text file, not included in the
    directory. Details listed in comments in code

--- AutoCWRUTurbine.py
+++ AutoCWRUTurbine.py
@@ -6,12 +6,24 @@ from time import localtime, strftime
 
 #Global variable setup
 res = ""
+idPath = "IDs.txt"
+
+#Fetching IDs from a text file located at idPath
+#Make sure that it is 1 ID per line, and an extra line break after weatherID
+ids = []
+with open(idPath) as f:
+    for line in f:
+        ids.append(line[:-1])
+tID1 = ids[0]
+tID2 = ids[1]
+tID3 = ids[2]
+tID4 = ids[3]
+weatherID = ids[4]
 
 #tweepy setup
-auth = tweepy.OAuthHandler("U0n6CZW67KNy2qs2Mf3J8nxrN", "ebBiqoGZPKqsNVLwKyYT34PQkwL51lLaR8ITL2I3JZtqGTGAv5")
-auth.set_access_token("1211360971412738048-4BCT5FTx2sD3XoWkZAwckQWLX2dxxp", "D07b7yCDOe7z611sEkVwN6m81KvFZC3s88cFBaluDgAkA")
+auth = tweepy.OAuthHandler(tID1, tID2)
+auth.set_access_token(tID3, tID4)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-
 try:
     api.verify_credentials()
     print("Authentication OK")
@@ -20,7 +32,7 @@ except:
 
 def getWindSpeed():
     #Pulling from https://home.openweathermap.org/
-    res = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=44106,us&APPID=6fe134ed57ff49a15780fc1fb0ae5953&units=imperial').json()
+    res = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=44106,us&APPID='+weatherID+'&units=imperial').json()
     return res['wind']['speed']
 
 def tweet():
@@ -33,14 +45,14 @@ def tweet():
                 #t = "["+time+"]" + " No WOOSH :("
                 t = "No WOOSH :("
                 print(t)
-                api.update_status(t)
+                #api.update_status(t)
             if 5 <= speed <= 100:
                 #t = "["+time+"]" + " WOOSH"
                 t = "WOOSH"
                 for i in range(1, int(speed / 2.5)):
                     t += " WOOSH"
                 print(t)
-                api.update_status(t)
+                #api.update_status(t)
         except Exception as e:
             print("!!Error in tweeting!!")
             print(e)
